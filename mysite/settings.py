0332f1fa97ada5ac6142db9682fe367bb482f6e1
loganwoudstra <loganwoudstra@gmail.com>
2022-03-09 17:56:42 -0700

    change database

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,8 +90,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
